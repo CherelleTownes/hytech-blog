58072dc49455285f5f255fbc22a8211533259a3c
Sbaayel <baayel@gmail.com>
2020-04-14 14:10:30 -0400

    completed create blog feature

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -7,8 +7,9 @@ const Product = (props) => {
         <>
             <Link className="product" to={`/products/${props._id}`}>
                 <img className="product-image" src={props.imgURL} alt={props.name} />
-                <div className="product-name">{props.name}</div>
-                <div className="price">{`$${props.price}`}</div>
+          <div className="product-name">{props.name}</div>
+          <div className="product-name">{props.sub_title}</div>
+                {/* <div className="price">{`$${props.price}`}</div> */}
             </Link>
         </>
     )
